0af6eace4f125b5ec103f5ba8d05129565f9ba63
SemaDev_ <nathan.mulpas@gmail.com>
2026-06-29 17:54:24 +0200

    Update bot activity name to 'SAISON 1 : COBBLEMON'

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Prend info sur le serveur",
+        name: "SAISON 1 : COBBLEMON",
         // Activity type number (0 = Playing).
         type: 0,
       },
